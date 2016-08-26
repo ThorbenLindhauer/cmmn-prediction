58795200da88bdcc034d58aba6cbb538895e3824
Askar Akhmerov <askar.akhmerov@camunda.com>
2016-08-26 10:51:05 +0200

    add emoji mapping

--- cockpit-plugin/src/main/resources/plugin-webapp/prediction-plugin/app/plugin.js
+++ cockpit-plugin/src/main/resources/plugin-webapp/prediction-plugin/app/plugin.js
@@ -2,10 +2,28 @@ define(['angular'], function(angular) {
     var ngModule = angular.module('cockpit.plugin.prediction-plugin', []);
 
     var CaseInteractionController = ["$scope", 'camAPI', "$http","Uri", function($scope, camAPI, $http,Uri) {
+        var eMap = {
+            0: '😢',
+            1: '😏',
+            2: '😔',
+            3: '😌',
+            4: '😁',
+            5: '😁'
+        };
+        $scope.mapEmoji = function (p) {
+            return eMap[Math.floor(p.probability * 100/20)];
+        };
+
         if ($scope.instance.id) {
             $http.get(Uri.appUri("plugin://prediction-plugin/:engine/predictions/" + $scope.instance.id))
                 .success(function(data) {
+
+                    for (var i = 0;i < data.length; i++) {
+                        var prediction = data[i];
+                        prediction.emoji = $scope.mapEmoji(prediction);
+                    }
                     $scope.predictions = data;
+
                 });
         }
     }];
